a148d5a1550c72008af140d7c226391cfc313ecc
EtienneDuv <etienne.duverney@viacesi.fr>
2020-10-16 11:19:45 +0200

    firebase setup

---
+++ functions/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+app.use(cors({ origin: true }));
+const env = require('dotenv');
+env.config();
+
+// FIREBASE MODULES
+const admin = require('firebase-admin');
+const functions = require('firebase-functions');
+
+app.set('view engine', 'ejs'); // TEMPLATE ENGINE
+app.use('/', express.static('./public')); // STATICS
+
+// ROUTES
+const controller = require('./api/routes');
+
+// FIRE CONTROLLERS
+controller(app)
+
+exports.app = functions.https.onRequest(app)
